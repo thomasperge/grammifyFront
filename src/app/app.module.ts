3eca660156e36020991bc60970bdd24d5d53e1aa
thomasperge <thomaskauffmant74@gmail.com>
2023-09-22 20:59:52 +0200

    feat: adding navbar + footer + comment code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InputComponent } from './input/input.component';
 import { OutputComponent } from './output/output.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     InputComponent,
-    OutputComponent
+    OutputComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,

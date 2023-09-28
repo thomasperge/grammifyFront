6bcbd7cd1fb3abcf51080621356d741753efa1fd
Thomasperge <thomaskauffmant74@gmail.com>
2023-09-28 22:38:56 +0200

    feat: add border when click

--- src/app/translate-lang/translate-lang.component.ts
+++ src/app/translate-lang/translate-lang.component.ts
@@ -6,10 +6,20 @@ import { Router } from '@angular/router';
   templateUrl: './translate-lang.component.html',
   styleUrls: ['./translate-lang.component.scss']
 })
+
 export class TranslateLangComponent {
+  allIdButton = ["fr", "en", "sp"];
   constructor(private router: Router) { }
 
   redirectToTranslatePage(language: string) {
     this.router.navigate(['/translator'], { queryParams: { lang: language } });
+
+    for(let i = 0; i <= this.allIdButton.length; i++) {
+      if (this.allIdButton[i] == language) {
+        document.getElementById(language)?.classList.add("border-blue-400")
+      } else {
+        document.getElementById(language)?.classList.add("border-gray-100")
+      }
+    }
   }
 }

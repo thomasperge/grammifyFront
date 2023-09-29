c7fc7e23b16208f3a85d3bc110fcba7fa96abe9a
Thomasperge <thomaskauffmant74@gmail.com>
2023-09-29 22:12:12 +0200

    comment: comment component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
-import { Router } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
 @Component({

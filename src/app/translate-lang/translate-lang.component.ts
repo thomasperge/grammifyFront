489e81733690546588f620989ab696820d1df621
Thomasperge <thomaskauffmant74@gmail.com>
2023-09-29 12:25:27 +0200

    feat: add border button

--- src/app/translate-lang/translate-lang.component.ts
+++ src/app/translate-lang/translate-lang.component.ts
@@ -14,12 +14,16 @@ export class TranslateLangComponent {
   redirectToTranslatePage(language: string) {
     this.router.navigate(['/translator'], { queryParams: { lang: language } });
 
-    for(let i = 0; i <= this.allIdButton.length; i++) {
-      if (this.allIdButton[i] == language) {
-        document.getElementById(language)?.classList.add("border-blue-400")
+    for(let i = 0; i < this.allIdButton.length; i++) {
+      const buttonId = this.allIdButton[i];
+      if (buttonId === language) {
+        document.getElementById(buttonId)?.classList.remove("border-gray-200")
+        document.getElementById(buttonId)?.classList.add("border-blue-500")
       } else {
-        document.getElementById(language)?.classList.add("border-gray-100")
+        document.getElementById(buttonId)?.classList.remove("border-blue-500")
+        document.getElementById(buttonId)?.classList.add("border-gray-200")
       }
     }
-  }
+}
+
 }

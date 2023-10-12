1c1ab65665f4b85da3a1731c4a5dc5fca484f10f
Thomasperge <thomaskauffmant74@gmail.com>
2023-10-12 17:19:26 +0200

    feat: finish front login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { initFlowbite } from 'flowbite';
 
 @Component({
@@ -9,6 +10,20 @@ import { initFlowbite } from 'flowbite';
 })
 export class AppComponent implements OnInit {
   title = 'GrammifyFront';
+  isDisplayNavBar = true;
+
+  constructor(private router: Router) {
+    // Check all changement route
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const currentRoute = this.router.url;
+        // Undisplay navbar if /login or /signup route
+        if (currentRoute == "/login" || currentRoute == "/signup") {
+          this.isDisplayNavBar = false
+        }
+      }
+    });
+  }
 
   ngOnInit(): void {
     initFlowbite();

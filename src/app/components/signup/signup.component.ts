4d93153102a93542365ffb682f97bb8a4b6c3f50
Thomasperge <thomaskauffmant74@gmail.com>
2023-10-12 17:43:23 +0200

    feat: adding signup page

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  constructor(private router: Router) {}
 
+  redirectToLoginPage() {
+    this.router.navigate(['/login']);
+  }
 }

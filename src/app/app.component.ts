d5ab830592286b39a42f54b0a10b1e1cffa5dbd4
Thomasperge <thomaskauffmant74@gmail.com>
2023-10-13 17:47:15 +0200

    fix: save userId LocalStorage if user edit his userId

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { initFlowbite } from 'flowbite';
@@ -14,6 +14,7 @@ import { UsagesService } from './services/usages.service';
 export class AppComponent implements OnInit {
   title = 'GrammifyFront';
   isDisplayNavBar = true;
+  protectedUserId = ""
 
   constructor(private router: Router, private unknownUserService: UnknownUserService, private usagesService: UsagesService) {
     // Check all changement route
@@ -41,8 +42,9 @@ export class AppComponent implements OnInit {
 
     // Check if user have localstorage userId
     if (userId) {
+      this.protectedUserId = userId
       // Get usages from id in local storage
-      let nbUsages = await this.unknownUserService.getUsageUnknownUser(userId)
+      let nbUsages = await this.unknownUserService.getUsageUnknownUser(this.protectedUserId)
 
       // User change id in the localStorage (response -1 = Bad request)
       if (nbUsages == -1) {
@@ -52,7 +54,6 @@ export class AppComponent implements OnInit {
         this.usagesService.setUsages(nbUsages)
       }
     } else {
-
       // Set id user in localStorage
       const newUserId = this.generateUuid();
       localStorage.setItem('userId', newUserId);
@@ -61,4 +62,12 @@ export class AppComponent implements OnInit {
       this.unknownUserService.createUnknownUser(newUserId)
     }
   }
+
+  // Set id local storage if user edit the id in localStorage
+  @HostListener('window:beforeunload', ['$event'])
+  onBeforeUnload(event: BeforeUnloadEvent): void {
+    if (localStorage.getItem('userId') != this.protectedUserId) {
+      localStorage.setItem('userId', this.protectedUserId);
+    }
+  }  
 }

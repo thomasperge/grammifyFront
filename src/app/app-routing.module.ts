c37dae951ecfd62e484a0ac7d89a9ae43af98de2
Thomasperge <thomaskauffmant74@gmail.com>
2023-10-20 21:11:08 +0200

    feat: adding dist ouputpath

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -26,3 +26,16 @@ const routes: Routes = [
 })
 
 export class AppRoutingModule { }
+
+// Degager couleur footer
+// navbar : space-beeween
+// Usages coler le 0/0
+// CopiCliboadr, background vlue
+
+
+// grammity.com
+// grammulate.com
+// grammscribe.com |
+// grammila.com
+// grammito.com
+// grammiti.com
